d36a7042b4c4d6b085e5bfca06462d856a8a2eb1
dongzhzheng <dongzhzheng@tencent.com>
2023-11-08 11:37:42 +0800

    feat: 切换到woa环境

--- bench.sh
+++ bench.sh
@@ -1,1 +1,1 @@
-go test -benchmem -run=^$ -bench ^Benchmark github.com/userpro/linearpool -v -count=1 -benchtime=3s
+go test -benchmem -run=^$ -bench ^Benchmark git.woa.com/trpc-vtproto/common/linearpool -v -count=1 -benchtime=3s
