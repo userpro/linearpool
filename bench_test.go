8e0a74deb83e37953e9d6f9be41907564d41b996
dongzhzheng <dongzhzheng@tencent.com>
2023-11-15 17:20:57 +0800

    feat: reformat benchmarks

--- bench_test.go
+++ bench_test.go
@@ -18,10 +18,9 @@ const objnum = 1000
 
 // BenchmarkStructRawAlloc ...
 func BenchmarkStructRawAlloc(b *testing.B) {
-	d1 := make([]*allocTest1, 0, 1)
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		d1 := make([]*allocTest1, 0, 1)
 		for i := 0; i < objnum; i++ {
 			d1 = append(d1, &allocTest1{
 				A: "123123123123",
@@ -31,17 +30,16 @@ func BenchmarkStructRawAlloc(b *testing.B) {
 				},
 			})
 		}
-		d1 = d1[:0]
 	}
 }
 
 // BenchmarkStructPoolAlloc ...
 func BenchmarkStructPoolAlloc(b *testing.B) {
 	ac := NewAlloctorFromPool(DiMB)
-	d1 := NewSlice[*allocTest1](ac, 0, 1)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		d1 := NewSlice[*allocTest1](ac, 0, 1)
 		for i := 0; i < objnum; i++ {
 			d := New[allocTest1](ac)
 			d.A = ac.NewString("123123123123")
@@ -51,86 +49,60 @@ func BenchmarkStructPoolAlloc(b *testing.B) {
 			d.C.D = ac.NewString("123123123123")
 			d1 = AppendMulti[*allocTest1](ac, d1, d)
 		}
-		d1 = d1[:0]
-	}
-}
-
-// BenchmarkStructPoolAllocOne ...
-func BenchmarkStructPoolAllocOne(b *testing.B) {
-	ac := NewAlloctorFromPool(DiMB)
-	d1 := NewSlice[*allocTest1](ac, 0, 1)
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		for i := 0; i < objnum; i++ {
-			d := New[allocTest1](ac)
-			d.A = ac.NewString("123123123123")
-			d.B = NewSlice[int32](ac, 0, 7)
-			d.B = AppendMulti[int32](ac, d.B, []int32{1, 2, 3, 4, 5, 6, 7}...)
-			d.C = New[allocTest2](ac)
-			d.C.D = ac.NewString("123123123123")
-			d1 = Append[*allocTest1](ac, d1, d)
-		}
-		d1 = d1[:0]
+		ac.Reset()
 	}
 }
 
 // BenchmarkIntSliceRawAlloc ...
 func BenchmarkIntSliceRawAlloc(b *testing.B) {
-	d1 := make([]int, 0, objnum)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		d1 := make([]int, 0, objnum)
 		for i := 0; i < objnum; i++ {
 			d1 = append(d1, i)
 		}
-		d1 = d1[:0]
 	}
 }
 
 // BenchmarkIntSlicePoolAllocAppendInplaceMulti ...
 func BenchmarkIntSlicePoolAllocAppendInplaceMulti(b *testing.B) {
 	ac := NewAlloctorFromPool(DiMB)
-	d1 := NewSlice[int](ac, 0, objnum)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		d1 := NewSlice[int](ac, 0, objnum)
 		for i := 0; i < objnum; i++ {
 			d1 = AppendInplaceMulti[int](ac, d1, i)
 		}
-		d1 = d1[:0]
+		ac.Reset()
 	}
 }
 
 // BenchmarkIntSlicePoolAllocAppendInplace ...
 func BenchmarkIntSlicePoolAllocAppendInplace(b *testing.B) {
 	ac := NewAlloctorFromPool(DiMB)
-	d1 := NewSlice[int](ac, 0, objnum)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		d1 := NewSlice[int](ac, 0, objnum)
 		for i := 0; i < objnum; i++ {
 			d1 = AppendInplace[int](ac, d1, i)
 		}
-		d1 = d1[:0]
+		ac.Reset()
 	}
 }
 
 // BenchmarkIntSlicePoolAllocAppendInbound ...
 func BenchmarkIntSlicePoolAllocAppendInbound(b *testing.B) {
 	ac := NewAlloctorFromPool(DiMB)
-	d1 := NewSlice[int](ac, 0, objnum)
-	// d2 := d1
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
+		d1 := NewSlice[int](ac, 0, objnum)
 		for i := 0; i < objnum; i++ {
 			d1 = AppendInbound[int](ac, d1, i)
 		}
-		d1 = d1[:0]
+		ac.Reset()
 	}
-
-	// h1 := (*sliceHeader)(unsafe.Pointer(&d1))
-	// h2 := (*sliceHeader)(unsafe.Pointer(&d2))
-	// assert.EqualValues(b, h1.Data, h2.Data)
 }
